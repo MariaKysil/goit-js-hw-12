4085754a6c97531dd81f06085e3742520f44594e
MariaKysil <marygoodee@gmail.com>
2021-07-25 19:26:07 +0300

    changes

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -10,14 +10,3 @@ export function fetchCountries(name) {
     },
   );
 }
-
-// export const fetchCountries = function (name) {
-//   const URL = 'https://restcountries.eu/rest/v2/name/';
-
-//   return fetch(`${URL}${name}?fields=name;capital;population;flag;languages`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// };
